955f701623ae36b08195d07fe0323b4fd9e03def
ilovetayninh <ilovetayninh@9782e551-2f26-6fa2-2e00-5d579a6d1274>
2014-12-03 06:56:31 +0000

    create new vesion

---
+++ tags/vesion 1.0/3_DAL/KhachHangController.cs
@@ -0,0 +1,182 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using _4_DTO;
+
+namespace _3_DAL
+{
+    public class KhachHangController
+    {
+        static QLNSModelDataContext db = new QLNSModelDataContext();
+
+        public static void InsertCustomerDAL(KHACHHANG temp)
+        {
+            db.KHACHHANGs.InsertOnSubmit(temp);
+
+            db.SubmitChanges();
+        }
+
+        public static DataTable SellectAllCustomerDAL()
+        {
+            DataTable dt = new DataTable();
+
+            var query = from item in db.KHACHHANGs
+                        where item.XoaDuLieu == false
+                        select new
+                        {
+                            item.MaKH,
+                            item.HoTen,
+                            item.DiaChi,
+                            item.DienThoai,
+                            item.Email,
+                            item.SoTienNo
+                        };
+            dt.Columns.Add("STT", typeof(int));
+            dt.Columns.Add("Mã KH", typeof(string));
+            dt.Columns.Add("Tên KH", typeof(string));
+            dt.Columns.Add("Địa Chỉ", typeof(string));
+            dt.Columns.Add("Số điện thoại", typeof(string));
+            dt.Columns.Add("Email", typeof(string));
+            dt.Columns.Add("Số tiền nợ", typeof(long));
+            int stt = 1;
+            foreach (var item in query)
+            {
+                dt.Rows.Add(stt, item.MaKH, item.HoTen, item.DiaChi, item.DienThoai, item.Email, item.SoTienNo);
+                stt++;
+            }
+
+            return dt;
+        }
+
+        //DataGridview select KH by MaKH
+        public static KHACHHANG SelectCustomerDAL(string key)
+        {
+            KHACHHANG query = db.KHACHHANGs.Single(i => i.MaKH.Equals(key));
+            return query;
+        }
+
+        public static void DeleteCustomersDAL(List<string> keys)
+        {
+            //Take custoners in KHACHHANGs which have MAKH
+
+            var queryHD = from item in db.HOADONs
+                          where keys.Contains(item.MaKH)
+                          select item;
+            
+            //Delete HOADON in CTHD
+            var queryCTHD = from item in db.CTHDs
+                            join hd in queryHD on item.MaHD equals hd.MaHD
+                            select new { item.MaHD};
+
+            List<string> lst = new List<string>();
+            foreach (var i in queryCTHD)
+            {
+                lst.Add(i.MaHD);
+            }
+
+            //Detele
+            db.CTHDs
+                .Where(i => lst.Contains(i.MaHD))
+                .ToList()
+                .ForEach(i => i.XoaDuLieu = true);
+            db.HOADONs
+                .Where(i => keys.Contains(i.MaKH))
+                .ToList()
+                .ForEach(i => i.XoaDuLieu = true);
+            db.CONGNOs
+                .Where(i => keys.Contains(i.MaKH))
+                .ToList()
+                .ForEach(i => i.XoaDuLieu = true);
+            db.KHACHHANGs
+                .Where(i => keys.Contains(i.MaKH))
+                .ToList()
+                .ForEach(i => i.XoaDuLieu = true);
+
+            //Confirm database
+            db.SubmitChanges();
+
+        }
+
+        public static bool checkMaKHDAL(string makh)
+        {
+            var query = from item in db.KHACHHANGs
+                        where item.MaKH.Equals(makh)
+                        select item;
+
+            if (query.Count() <= 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        //kiểm tra lại
+        public static void UpdateCustomerDAL(KHACHHANG item)
+        {
+            var query = db.KHACHHANGs.Single(sa => sa.MaKH == item.MaKH);
+            query.HoTen = item.HoTen;
+            query.DiaChi = item.DiaChi;
+            query.DienThoai = item.DienThoai;
+            query.Email = item.Email;
+            query.SoTienNo = item.SoTienNo;
+
+            db.SubmitChanges();
+        }
+
+        public static DataTable SearchCustomersDAL(string temp)
+        {
+            var query = from item in db.KHACHHANGs
+                        where
+                        (
+                           item.MaKH.StartsWith(temp) ||
+                           item.MaKH.EndsWith(temp) ||
+                           item.MaKH.Contains(temp) ||
+                           item.HoTen.StartsWith(temp) ||
+                           item.HoTen.EndsWith(temp) ||
+                           item.HoTen.Contains(temp) ||
+                           item.DiaChi.StartsWith(temp) ||
+                           item.DiaChi.EndsWith(temp) ||
+                           item.DiaChi.Contains(temp) ||
+                           item.DienThoai.StartsWith(temp) ||
+                           item.DienThoai.EndsWith(temp) ||
+                           item.DienThoai.Contains(temp) ||
+                           item.Email.StartsWith(temp) ||
+                           item.Email.EndsWith(temp) ||
+                           item.Email.Contains(temp) ||
+                           item.SoTienNo.ToString().Contains(temp) ||
+                           item.SoTienNo.ToString().StartsWith(temp) ||
+                           item.SoTienNo.ToString().EndsWith(temp)
+                        ) && item.XoaDuLieu == false
+                        select new
+                        {
+                            item.MaKH,
+                            item.HoTen,
+                            item.DiaChi,
+                            item.DienThoai,
+                            item.Email,
+                            item.SoTienNo
+                        };
+
+
+            DataTable dt = new DataTable();
+            dt.Columns.Add("STT", typeof(int));
+            dt.Columns.Add("Mã KH", typeof(string));
+            dt.Columns.Add("Tên KH", typeof(string));
+            dt.Columns.Add("Địa Chỉ", typeof(string));
+            dt.Columns.Add("Số điện thoại", typeof(string));
+            dt.Columns.Add("Email", typeof(string));
+            dt.Columns.Add("Số tiền nợ", typeof(long));
+            int stt = 1;
+            foreach (var item in query)
+            {
+                dt.Rows.Add(stt, item.MaKH, item.HoTen, item.DiaChi, item.DienThoai, item.Email, item.SoTienNo);
+                stt++;
+            }
+
+            return dt;
+        }
+    }
+}
---
+++ tags/vesion 1.0/3_DAL/TonController.cs
@@ -0,0 +1,186 @@
+﻿using _4_DTO;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System;
+
+namespace _3_DAL
+{
+    public class TonController
+    {
+        static QLNSModelDataContext db = new QLNSModelDataContext();
+       
+        public static void InsertTonDAL(PHIEUTON item)
+        {
+            db.PHIEUTONs.InsertOnSubmit(item);
+            db.SubmitChanges();
+        }
+
+        public static DataTable SellectAllTonsDAL()
+        {
+            DataTable dt = new DataTable();
+
+            var query = from item in db.PHIEUTONs
+                        join item1 in db.SACHes on item.MaSach equals item1.MaSach
+                        where item.XoaDuLieu == false
+                        select new
+                        {
+                            item.MaTon,
+                            item1.TenSach,
+                            item.TonDau,
+                            item.TonPhatSinh,
+                            item.TonCuoi,
+                            item.Thang
+                        };
+            dt.Columns.Add("STT", typeof(int));
+            dt.Columns.Add("Mã Tồn", typeof(string));
+            dt.Columns.Add("Tên Sách", typeof(string));
+            dt.Columns.Add("Tồn Đầu", typeof(int));
+            dt.Columns.Add("Tồn Phát Sinh", typeof(int));
+            dt.Columns.Add("Tồn Cuối", typeof(int));
+            dt.Columns.Add("Tháng", typeof(int));
+            int stt = 1;
+            foreach (var item in query)
+            {
+                dt.Rows.Add(stt, item.MaTon, item.TenSach, item.TonDau, item.TonPhatSinh, item.TonCuoi, item.Thang);
+                stt++;
+            }
+
+            return dt;
+        }
+
+        public static PHIEUTON SelectTonDAL(string key)
+        {
+            PHIEUTON query = db.PHIEUTONs.Single(i => i.MaTon.Equals(key));
+            return query;
+        }
+
+        public static void DeleteTonsDAL(List<string> keys)
+        {
+            //Detele
+            db.PHIEUTONs
+                .Where(i => keys.Contains(i.MaTon))
+                .ToList()
+                .ForEach(i => i.XoaDuLieu = true);
+
+            //Confirm database
+            db.SubmitChanges();
+
+        }
+
+        public static bool checkMaTonDAL(string key)
+        {
+            var query = from item in db.PHIEUTONs
+                        where item.MaTon.Equals(key)
+                        select item;
+
+            if (query.Count() <= 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static void UpdateTonDAL(PHIEUTON item)
+        {
+            var query = db.PHIEUTONs.Single(i => i.MaTon == item.MaTon);
+            query.MaTon = item.MaTon;
+            query.TonDau = item.TonDau;
+            query.TonPhatSinh = item.TonPhatSinh;
+            query.TonCuoi = item.TonCuoi;
+            query.MaSach = item.MaSach;
+            query.Thang = item.Thang;
+
+            db.SubmitChanges();
+        }
+
+        public static DataTable SearchTonsDAL(string key)
+        {
+            var query = from item in db.PHIEUTONs
+                        join item1 in db.SACHes on item.MaSach equals item1.MaSach
+                        where
+                        (
+                           item.MaTon.StartsWith(key) ||
+                           item.MaTon.EndsWith(key) ||
+                           item.MaTon.Contains(key) ||
+                           item1.TenSach.StartsWith(key) ||
+                           item1.TenSach.EndsWith(key) ||
+                           item1.TenSach.Contains(key) ||
+                           item.TonDau.ToString().StartsWith(key) ||
+                           item.TonDau.ToString().EndsWith(key) ||
+                           item.TonDau.ToString().Contains(key) ||
+                           item.TonPhatSinh.ToString().StartsWith(key) ||
+                           item.TonPhatSinh.ToString().EndsWith(key) ||
+                           item.TonPhatSinh.ToString().Contains(key) ||
+                           item.TonCuoi.ToString().StartsWith(key) ||
+                           item.TonCuoi.ToString().EndsWith(key) ||
+                           item.TonCuoi.ToString().Contains(key) ||
+                           item.Thang.ToString().StartsWith(key) ||
+                           item.Thang.ToString().EndsWith(key) ||
+                           item.Thang.ToString().Contains(key)
+                        ) && item.XoaDuLieu == false
+                        select new
+                        {
+                            item.MaTon,
+                            item1.TenSach,
+                            item.TonDau,
+                            item.TonPhatSinh,
+                            item.TonCuoi,
+                            item.Thang
+                        };
+
+            DataTable dt = new DataTable();
+            dt.Columns.Add("STT", typeof(int));
+            dt.Columns.Add("Mã Tồn", typeof(string));
+            dt.Columns.Add("Tên Sách", typeof(string));
+            dt.Columns.Add("Tồn Đầu", typeof(int));
+            dt.Columns.Add("Tồn Phát Sinh", typeof(int));
+            dt.Columns.Add("Tồn Cuối", typeof(int));
+            dt.Columns.Add("Tháng", typeof(int));
+            int stt = 1;
+            foreach (var item in query)
+            {
+                dt.Rows.Add(stt, item.MaTon, item.TenSach, item.TonDau, item.TonPhatSinh, item.TonCuoi, item.Thang);
+                stt++;
+            }
+
+            return dt;
+        }
+
+        public static DataTable SelectTon_MonthDAL(string key)
+        {
+            DataTable dt = new DataTable();
+
+            var query = from item in db.PHIEUTONs
+                        join item1 in db.SACHes on item.MaSach equals item1.MaSach
+                        where item.Thang == Convert.ToInt16(key)
+                        select new
+                        {
+                            item.MaTon,
+                            item1.TenSach,
+                            item.TonDau,
+                            item.TonPhatSinh,
+                            item.TonCuoi,
+                            item.Thang
+                        };
+            dt.Columns.Add("STT", typeof(int));
+            dt.Columns.Add("Mã Tồn", typeof(string));
+            dt.Columns.Add("Tên Sách", typeof(string));
+            dt.Columns.Add("Tồn Đầu", typeof(int));
+            dt.Columns.Add("Tồn Phát Sinh", typeof(int));
+            dt.Columns.Add("Tồn Cuối", typeof(int));
+            dt.Columns.Add("Tháng", typeof(int));
+            int stt = 1;
+            foreach (var item in query)
+            {
+                dt.Rows.Add(stt, item.MaTon, item.TenSach, item.TonDau, item.TonPhatSinh, item.TonCuoi, item.Thang);
+                stt++;
+            }
+
+            return dt;
+        }
+    }
+}
