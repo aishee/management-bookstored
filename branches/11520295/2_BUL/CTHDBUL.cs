bc5a1e31204e044b933335a28ef44ea51ed3e961
11520295@gm.uit.edu.vn <11520295@gm.uit.edu.vn@9782e551-2f26-6fa2-2e00-5d579a6d1274>
2014-12-16 05:32:31 +0000

    Phương

--- branches/11520295/2_BUL/CTHDBUL.cs
+++ branches/11520295/2_BUL/CTHDBUL.cs
@@ -4,14 +4,56 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using _3_DAL;
+using _4_DTO;
+using System.Data;
 
 namespace _2_BUL
 {
     public class CTHDBUL
     {
-        public static void DeleteCTHDsBUL(List<string> cthds)
+        public static DataTable SellectAllCTHDsBUL()
         {
-            CTHDDAL.DeleteCTHDsDAL(cthds);
+            return CTHDController.SellectAllCTHDsDAL();
+        }
+
+        public static void DeleteCTHDsBUL(List<string> keys)
+        {
+            CTHDController.DeleteCTHDsDAL(keys);
+        }
+
+        public static CTHD SelectCTHDBUL(string key)
+        {
+            return CTHDController.SelectCTHDDAL(key);
+        }
+
+        public static void InsertCTHDBUL(CTHD item)
+        {
+            CTHDController.InsertCTHDDAL(item);
+        }
+
+        public static bool checkMaCTHDBUL(string key)
+        {
+            return CTHDController.checkMaCTHDDAL(key);
+        }
+
+        public static void UpdateCTHDBUL(CTHD item)
+        {
+            CTHDController.UpdateCTHDDAL(item);
+        }
+
+        public static DataTable SearchCTHDsBUL(string key)
+        {
+            return CTHDController.SearchCTHDsDAL(key);
+        }
+
+        public static bool checkKH_CTHDBUL(string key)
+        {
+            return CTHDController.checkKH_CTHDDAL(key);
+        }
+
+        public static int? TakeSach_SL_TonBUL(string key)
+        {
+            return CTHDController.TakeSach_SL_TonDAL(key);
         }
     }
 }
